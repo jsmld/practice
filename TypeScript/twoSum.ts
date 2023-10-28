e4cea8c228c8f23c5feec35d8c231e2031fad868
jsmld <samvanerpersroijaards@gmail.com>
2023-10-28 12:30:48 -0300

    Understanding Maps now lol

--- TypeScript/twoSum.ts
+++ TypeScript/twoSum.ts
@@ -18,38 +18,20 @@ function twoSum(nums: number[], target: number): number[] {
   return [-1]
 };
 
-// Here's a fancy solution I found with a good explanation. Once I understand Map a bit better,
-// I'll remove all these comments.
+// Here's a fancy solution I found which uses a Map, loops over the numbers array and creates a `difference`
+// using the current number in the array and target. It then accesses the Map to check if this difference exists already
+// then returns an array of the index of the difference and the index of the current number; otherwise it sets the current number
+// and its index in the Map
+
+// Returning [-1] at the end resolves the return type 
 
 function fancyTwoSum(numbers: number[], target: number): number[] {
-  // create a map as a method of key value storage for O(1) search / access
-  // Array has an O(n) search-time, so we opt for the map
   const numberMap = new Map()
-
-  // start by looping through each number (we use a traditional
-  // for-loop here since it allows us to use "break" or "return" if we find
-  // the number we're looking for before reaching the end of the array)
   for (let i = 0; i < numbers.length; i++) {
       const number = numbers[i]
-
-      // here we find the difference between the target and number...
-      // "difference" represents the number that needs to be added
-      // onto "number" (the current number we're looping over) in order
-      // to equal the function's target number
       const difference = target - number
-
-      // check numberMap for the difference value. if we already
-      // looped over this number, that means it's in our map, therefore
-      // we can retrieve the value from our map (O(1)), get its index, and return
-      // the index we're currently looping over to complete the problem
       if (numberMap.has(difference)) return [numberMap.get(difference), i]
-
-      // if we haven't already looped over the number, store it
-      // and its index in a JS Map (hash table) so we can
-      // get an O(1) search / access check to see if we looped over the
-      // number previously
       numberMap.set(number, i)
   }
-  // I added this to satisfy the return type
   return [-1]
 };

0bd1af7543e4d1f2759a1d2e704380c8c4291b1b
jsmld <samvanerpersroijaards@gmail.com>
2023-11-08 23:29:50 -0300

    Understanding groupAnagram algorithm a bit more

--- groupAnagrams.ts
+++ groupAnagrams.ts
@@ -1,5 +1,11 @@
+// Given an array of strings, it will find anagrams and return them grouped together.
+
+// First off, we create a new Map. Then for each of the strings in the strs array we create an array called count
+// (initialized with 26 0's representing a placeholder for each letter of the alphabet). From there we take each string
+// and for each character we calculate its position in the count array through Unicode magic. I'm still a bit tripped up
+// by line 14. Needs a bit more study but it's coming together in my head now more than before.
 function groupAnagrams(strs: string[]): string[][] {
-  const res = new Map();
+  const anagramMap = new Map();
 
   for (const str of strs) {
       const count = Array(26).fill(0);
@@ -9,12 +15,12 @@ function groupAnagrams(strs: string[]): string[][] {
       }
 
       const key = count.join(",");
-      if (!res.has(key)) {
-          res.set(key, []);
+      if (!anagramMap.has(key)) {
+          anagramMap.set(key, []);
       }
-      res.get(key).push(str);
+      anagramMap.get(key).push(str);
   }
 
-  return Array.from(res.values());
+  return Array.from(anagramMap.values());
 
 };

f26d6b1a903cd0349af762c7a3d4e4447e9a3ec7
jsmld <samvanerpersroijaards@gmail.com>
2023-11-11 15:12:13 -0300

    I think I understand topKFrequent now, needs a lot of practice

--- topKFrequent.ts
+++ topKFrequent.ts
@@ -5,6 +5,7 @@ function topKFrequent(nums: number[], k: number): number[] {
   let bucket: Set<number>[] = [];
   let result: number[] = [];
 
+  // This gets each of the numbers in nums and the amount they occur.
   for (let i = 0; i < nums.length; i++) {
     if (!map[nums[i]]) {
       map[nums[i]] = 1;
@@ -12,7 +13,9 @@ function topKFrequent(nums: number[], k: number): number[] {
       map[nums[i]]++;
     }
   }
-
+  // This is the crucial part where we organize the numbers into sets based on their frequency.
+  // If it's the first time we encounter a frequency, we create a new set; otherwise, we add
+  // the number to the existing set with that frequency.
   for (let [numStr, freq] of Object.entries(map)) {
     let num = parseInt(numStr, 10);
     if (!bucket[freq]) {
@@ -21,7 +24,10 @@ function topKFrequent(nums: number[], k: number): number[] {
       bucket[freq].add(num);
     }
   }
-
+  // We work backwards from the end of the array (since we set up the bucket to have the highest
+  // frequency at the end). From there we check if there is a set at the current index and if so
+  // we convert the set into an array and push its element/elements into the result array. Then we
+  // check if the length of the results array matches the k value and break if it does. 
   for (let i = bucket.length - 1; i >= 0; i--) {
     if (bucket[i]) result.push(...Array.from(bucket[i]));
     if (result.length === k) break;
